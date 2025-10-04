d691afb0467ed1b0cc61388e2cd14d31c4616ba2
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-05 01:37:42 +0900

    Fix dark mode surfaces and improve accessibility

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import router from './router';
 import { i18n } from './locales';
-import './styles/main.css';
 import 'primevue/resources/themes/aura-light-noir/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
+import './styles/main.css';
 
 const app = createApp(App);
 
