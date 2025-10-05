06130fbfe903af189f7d4ca841d81ae5ce04244d
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-05 19:47:39 +0900

    refactor: centralize shared component styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ import primeui from 'tailwindcss-primeui'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./playground/index.html', './playground/src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './playground/index.html',
+    './playground/src/**/*.{vue,js,ts,jsx,tsx}',
+    './library/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   darkMode: 'class',
   theme: {
     extend: {
