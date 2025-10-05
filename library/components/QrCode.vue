3993678f6d968996ba07a7621b15315d38bff1f4
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-05 23:54:40 +0900

    Update QrCode.vue

--- library/components/QrCode.vue
+++ library/components/QrCode.vue
@@ -60,7 +60,7 @@ updateQrCode()
 
 <template>
   <section class="relative aspect-square items-center justify-center rounded-3xl border border-surface-200 bg-surface-0 p-4 shadow-sm">
-    <img v-if="!qrError" :src="qrSource" alt="QR code" class="w-full object-contain" />
+    <img v-if="!qrError" :src="qrSource" alt="QR code" class="w-full object-contain" style="image-rendering: crisp-edges; image-rendering: pixelated;" />
     <div v-else class="flex h-full w-full items-center justify-center text-xs text-center">
       <span>{{ qrError }}</span>
     </div>
