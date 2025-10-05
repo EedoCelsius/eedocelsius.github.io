0df83732d5bc359f49481d33e185098f40d98483
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-05 18:45:56 +0900

    Update LoadingOverlay.vue

--- src/components/library/LoadingOverlay.vue
+++ src/components/library/LoadingOverlay.vue
@@ -3,7 +3,7 @@ export const defaultProps = {
   visible: true,
   blur: 7,
   description: '',
-  overlayColor: 'rgba(0, 0, 0, 0)',
+  overlayColor: 'rgba(0, 0, 0, 0.25)',
   spinnerSize: 48,
   spinnerThickness: 4,
   spinnerTrackColor: 'color-mix(in srgb, var(--p-surface-0) 25%, transparent)',
