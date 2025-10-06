6d3110c8dd6f16437d87598e07975cf9645ffa74
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-07 01:36:00 +0900

    Revert "feat: add ActionableCard component"

--- playground/src/views/core/controls/field.vue
+++ playground/src/views/core/controls/field.vue
@@ -2,7 +2,6 @@
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import Checkbox from 'primevue/checkbox'
-import Dropdown from 'primevue/dropdown'
 import InputText from 'primevue/inputtext'
 import Slider from 'primevue/slider'
 import Textarea from 'primevue/textarea'
@@ -43,13 +42,6 @@ const stringModel = computed<string | undefined>({
   get: () => (typeof valueModel.value === 'string' ? valueModel.value : undefined),
   set: (value) => { valueModel.value = value },
 })
-
-const selectOptions = computed(() =>
-  props.control.options?.map((option) => ({
-    label: localize(option.label),
-    value: option.value,
-  })) ?? []
-)
 
 const colorModel = computed<string | null>({
   get: () => toRgba(stringModel.value),
@@ -95,19 +87,6 @@ const booleanModel = computed<boolean | undefined>({
         v-model="stringModel"
         :aria-labelledby="isOptional ? labelId : undefined"
         class="w-full"
-      />
-    </template>
-    <template v-else-if="control.type === 'select'">
-      <Dropdown
-        v-if="isActive"
-        :id="inputId"
-        v-model="valueModel"
-        :options="selectOptions"
-        option-label="label"
-        option-value="value"
-        class="w-full"
-        :aria-labelledby="isOptional ? labelId : undefined"
-        :show-clear="true"
       />
     </template>
     <template v-else-if="control.type === 'textarea'">
