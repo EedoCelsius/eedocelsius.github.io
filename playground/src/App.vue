7903248472297213821a367fe10ae48e7b5242c7
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-06 21:59:50 +0900

    Revert "Revert "Revert "refactor: remove unnecessary keys from playground"""

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -131,7 +131,7 @@ watch(
     </header>
 
     <main class="mx-auto w-full max-w-6xl px-6 pb-16 pt-10">
-      <RouterView />
+      <RouterView :key="route.fullPath" />
     </main>
   </div>
 </template>
