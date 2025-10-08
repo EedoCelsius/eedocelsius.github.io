f77a95122c184a6f5b5c6cb9aafc40a9d39f6c57
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-08 22:07:33 +0900

    Update App.vue

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -100,8 +100,8 @@ watch(
 </script>
 
 <template>
-  <div class="min-h-screen px-4">
-    <header class="sticky top-0 z-40 max-w-6xl mx-auto pt-4">
+  <div class="min-h-screen p-4">
+    <header class="sticky top-0 z-40 max-w-6xl mx-auto">
       <Toolbar
         :pt="{
           root: {
